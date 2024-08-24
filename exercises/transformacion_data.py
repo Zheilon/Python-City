d1809eb8449d77e8837c3649b3e8fb956019b2ac
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-23 19:36:58 -0500

    New file: transformation_data.py

---
+++ exercises/transformacion_data.py
@@ -0,0 +1,6 @@
+user = str(input("Digíte su nombre completo: "))
+cedula = int(input("Digíte su Cédula: "))
+estatura = float(input("Digíte su Altura en Mts: "))
+estado = bool(input("Estado (True | False): "))
+
+print(f"Usuario: {user}\nCédula: {cedula}\nEstatura: {estatura}\nEstado Civil: {estado}")
