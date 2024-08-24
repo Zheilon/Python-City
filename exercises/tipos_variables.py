6453a05cbc3262a5a7536518e98a8ae77615878f
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-23 19:29:19 -0500

    Tipos de variables, Ejemplos.

---
+++ exercises/tipos_variables.py
@@ -0,0 +1,14 @@
+cadena = "GitHub"
+print(type(cadena))
+
+number = 555
+print(type(number))
+
+numberF = 5.55
+print(type(numberF))
+
+boolean = False
+print(type(boolean))
+
+char = 'J'
+print(type(char))
