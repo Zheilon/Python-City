25ee2e89da58a2d672bd1563f83d1906d18d03e6
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-23 19:39:24 -0500

    Uso de input, str, int, float, bool.

--- exercises/transformacion_data.py
+++ exercises/transformacion_data.py
@@ -3,4 +3,6 @@ cedula = int(input("Digíte su Cédula: "))
 estatura = float(input("Digíte su Altura en Mts: "))
 estado = bool(input("Estado (True | False): "))
 
+print()
+
 print(f"Usuario: {user}\nCédula: {cedula}\nEstatura: {estatura}\nEstado Civil: {estado}")
