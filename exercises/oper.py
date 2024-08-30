5ab3de6a129b6c035b95aad7779f2c40cb8ec68e
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-30 17:01:44 -0500

    New Variable!

--- exercises/oper.py
+++ exercises/oper.py
@@ -19,3 +19,6 @@ print(f"4TO Resultado: {d}")
 e = 15/3 * (7 + (68 - 15 * 33 + (math.pow(45, 2) / 5) / 3) / 2) + 19
 print(f"5TO Resultado: {e}")
 
+f = (4 * ((((4 + 8 * 2) / 4 - math.pow(3, 2) + math.sqrt(4)) / 2) * math.sqrt(25)) - 3) + 16
+print(f"6TO Resultado: {f}")
+
