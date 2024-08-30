ddaa99fd3ca99a8c1fef57b6ae536bddb019a31e
Juan Esteban Torres <104040670+Zheilon@users.noreply.github.com>
2024-08-30 15:37:08 -0500

    Add new Variable

--- exercises/oper.py
+++ exercises/oper.py
@@ -1,8 +1,14 @@
 import math
 
+#Ejercicio 1
 a = 5 * ( 2 + 5)
 print(f"Resultado: {a}")
 
+#Ejercicio 2
 b = (4 + 3) - (3 * 2) + 1
 print(f"2Do Resultado: {b}")
+
+#Ejercicio 3
+c = (2 * 3) + (5 * (9 / 3))
+print(f"3ER Resultado: {c}") 
 
