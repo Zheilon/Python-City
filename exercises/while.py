babfd4f07d99f8d7238f13ff767a94f498f82a11
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-09-13 17:09:55 -0500

    Calculadora con While Structures

--- exercises/while.py
+++ exercises/while.py
@@ -3,6 +3,7 @@ import os
 import math
 os.system('cls')
 print("!-------* Calculadora *-------!")
+print()
 
 while True:
     print("Suma -----------------> ( 1 )")
@@ -17,6 +18,12 @@ while True:
     two = float(input("Ingrese Segundo Dígito: "))
     print()
 
+    try:
+        result = one / two
+    except ZeroDivisionError as z:
+        print(f"{z}")
+
+
     if opcion == 1:
         print(f"Suma [ {one} + {two} ] = {one + two}")
 
@@ -26,15 +33,12 @@ while True:
     elif opcion == 3:
         print(f"Multiplicación [ {one} * {two} ] = {one * two}")
 
-    elif opcion == 4 and one != 0:
+    elif opcion == 4 and two != 0:
         print(f"División [ {one} / {two} ] = {one  / two}")
 
     elif opcion == 5:
         squrtT = int(input("Ingresa Digito a Operar con Raíz: "))
         print(f"Raiz Cuadrada [ {squrtT} ] = {math.sqrt(squrtT)}")
-
-    else:
-        print("No se puede Dividir por Cero")
 
     confirm = str(input("Continue? S / N: " ))
 
