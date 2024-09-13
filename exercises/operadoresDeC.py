66074d38d86b531755d089350cbf91ceb1fd7eb5
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-09-13 15:21:57 -0500

    Operadores de Comparación.

--- exercises/operadoresDeC.py
+++ exercises/operadoresDeC.py
@@ -7,15 +7,6 @@ num2 = 2
 print(num1 == num2)
 print(num1 != num2)
 print(num1 < num2)
-print(num > num2)
+print(num1 > num2)
 print(num1 <= num2)
 print(num1 >= num2) 
-
-print(" -- Program --")
-count = int(input("N° Estudiantes: "))
-if count > 10:
-    print("P")
-
-elif count > 20:
-    print("G")
-    
