16f4ad4bb33c46a3c0e78371cbdf82201d46e492
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-09-13 15:02:57 -0500

    Operadores de Comparación

---
+++ exercises/operadoresDeC.py
@@ -0,0 +1,21 @@
+import os
+os.system('cls')
+#Crear Variables
+num1 = 2
+num2 = 2
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 < num2)
+print(num > num2)
+print(num1 <= num2)
+print(num1 >= num2) 
+
+print(" -- Program --")
+count = int(input("N° Estudiantes: "))
+if count > 10:
+    print("P")
+
+elif count > 20:
+    print("G")
+    
