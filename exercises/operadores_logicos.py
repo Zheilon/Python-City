776da0a7856dfc13997d95dab4e396e1f4795771
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-09-13 15:19:47 -0500

    Operadores Logicos.

---
+++ exercises/operadores_logicos.py
@@ -0,0 +1,12 @@
+one = 1
+two = 2
+
+print(one == two and one <= two)
+print(one == two and one > two)
+print(one < two and one > two)
+print(one < two and one >= two)
+
+user = "perro"
+passW = "cemento12"
+
+print(user == "perro" and passW == "cemento12")
