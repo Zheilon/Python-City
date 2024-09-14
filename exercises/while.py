11d7c012798a926ee3ef283b83878895860a224e
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-09-13 20:18:44 -0500

    Calculator While -v 0.0.2

--- exercises/while.py
+++ exercises/while.py
@@ -16,36 +16,40 @@ while True:
 
     try:
         if opcion == 1:
+            print("------------*-< Suma >-*------------")
             one = float(input("Ingrese Primer Dígito: "))
             two = float(input("Ingrese Segundo Dígito: "))
             print(f"Suma [ {one} + {two} ] = {one + two}")
 
         elif opcion == 2:
+            print("------------*-< Resta >-*------------")
             one = float(input("Ingrese Primer Dígito: "))
             two = float(input("Ingrese Segundo Dígito: "))
             print(f"Resta [ {one} - {two} ] = {one - two}")
 
         elif opcion == 3:
+            print("------------*-< Multiplicación >-*------------")
             one = float(input("Ingrese Primer Dígito: "))
             two = float(input("Ingrese Segundo Dígito: "))
             print(f"Multiplicación [ {one} * {two} ] = {one * two}")
 
         elif opcion == 4:
+            print("------------*-< División >-*------------")
             one = float(input("Ingrese Primer Dígito: "))
             two = float(input("Ingrese Segundo Dígito: "))
             print(f"División [ {one} / {two} ] = {one  / two}")
             print()
 
         elif opcion == 5:
+            print("------------*-< Raiz Cuadrada >-*------------")
             squrtT = int(input("Ingresa Digito a Operar con Raíz: "))
 
-            if squrtT > 0:
-                print(f"Raiz Cuadrada [ {squrtT} ] = {math.sqrt(squrtT)}")
+            try: 
+                if squrtT > 0:
+                    print(f"Raiz Cuadrada [ {squrtT} ] = {math.sqrt(squrtT)}")
 
-            try:
-                math.sqrt(squrtT)
             except Exception as ex:
-                print(f"{ex}: El radicando no puede ser negativo!")
+                print(f"{ex}: El radicando no puede ser Negativo!")
 
     except Exception as z:
         print(f"{z}")
@@ -57,9 +61,7 @@ while True:
     if confirm.upper() == 'N':
         break
 
+    os.system('cls')
+
 print()
 print("Afuera\n")
-
-    
-
-
