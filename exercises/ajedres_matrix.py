d48faa466f52d3e6f1688c0739773f03e8f03658
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-10-12 23:23:56 -0500

    Algoritmo Juego Matricial: -V 0.4

--- exercises/ajedres_matrix.py
+++ exercises/ajedres_matrix.py
@@ -1,29 +1,33 @@
+import os
+os.system("cls")
+
 x = int(input("Ingresa Número de [ Filas ]: "))
 y = int(input("Ingresa Número de [ Columnas ]: "))
-increase = 0
 
 #!Problema Pricipal: Listas con mismo ID!
 #*Solución: Crear objetos con ID's distintos!
 
 initMatrix = [["w" for _ in range(y)] for _ in range(x)]
 
+
 def logic():
 
     for audi in range(len(initMatrix)):
-        for lamborguini in range(len(initMatrix[audi])):
+        for lamborguini in range(len(initMatrix[0])):
+
             if audi % 2 == 0:
                 if lamborguini % 2 == 0: 
-                    initMatrix[audi][lamborguini] = "N"
+                    initMatrix[audi][lamborguini] = "0"
 
                 elif lamborguini % 2 == 1:
-                    initMatrix[audi][lamborguini] = "B"
+                    initMatrix[audi][lamborguini] = "1"
 
             if audi % 2 == 1:
                 if lamborguini % 2 == 0:
-                    initMatrix[audi][lamborguini] = "B"
+                    initMatrix[audi][lamborguini] = "1"
                 
                 elif lamborguini % 2 == 1:
-                    initMatrix[audi][lamborguini] = "N"  
+                    initMatrix[audi][lamborguini] = "0"  
 logic()   
 
 
@@ -32,7 +36,7 @@ def show():
     for z in range(len(initMatrix)):
         print(end=f"\nF{z + 1}. ")
 
-        for w in range(len(initMatrix[z])):
+        for w in range(len(initMatrix[0])):
 
             if w % 2 == 0:
                 print(end=f"| {initMatrix[z][w]} |")
@@ -47,6 +51,7 @@ def show():
             print()
 show()
 
+
 print("\nBuscar Posición Matricial!")
 
 while True:
@@ -56,7 +61,7 @@ while True:
 
     if x <= len(initMatrix) and y <= len(initMatrix[0]):
         for camaro in range(len(initMatrix)):
-            for astonMartin in range(len(initMatrix[camaro])):
+            for astonMartin in range(len(initMatrix[0])):
 
                 if camaro == (x - 1) and astonMartin == (y - 1):
                     initMatrix[camaro][astonMartin] = initMatrix[camaro][astonMartin]
@@ -65,7 +70,7 @@ while True:
                     initMatrix[camaro][astonMartin] = "-"
         show()
         print(f"[{x}, {y}]")   
-
+    
     else:
         print("\nIngresa Rangos Permitidos!")
 
