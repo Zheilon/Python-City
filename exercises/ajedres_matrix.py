814d6f9a9a01401ff758c8de0d84ff536334ba3a
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-10-12 21:48:02 -0500

    Algoritmo Juego Matricial: -V 0.3

--- exercises/ajedres_matrix.py
+++ exercises/ajedres_matrix.py
@@ -1,37 +1,72 @@
-initMatrix = []
-listPar = []
-listInPar = []
-
 x = int(input("Ingresa Número de [ Filas ]: "))
 y = int(input("Ingresa Número de [ Columnas ]: "))
+increase = 0
 
-#*--- + --- + --- + ---\ LÓGICA \--- + --- + --- + ---*#
-for z in range(x):
-    if z % 2 == 0:
-        initMatrix.append(listPar)
-    elif z % 2 == 1:
-        initMatrix.append(listInPar)
+#!Problema Pricipal: Listas con mismo ID!
+#*Solución: Crear objetos con ID's distintos!
 
-for z in range(y):
-    if z % 2 == 0:
-        listPar.append("N")
-        listInPar.append("B")
-    elif z % 2 == 1:
-        listPar.append("B")
-        listInPar.append("N")
+initMatrix = [["w" for _ in range(y)] for _ in range(x)]
+
+def logic():
+
+    for audi in range(len(initMatrix)):
+        for lamborguini in range(len(initMatrix[audi])):
+            if audi % 2 == 0:
+                if lamborguini % 2 == 0: 
+                    initMatrix[audi][lamborguini] = "N"
+
+                elif lamborguini % 2 == 1:
+                    initMatrix[audi][lamborguini] = "B"
+
+            if audi % 2 == 1:
+                if lamborguini % 2 == 0:
+                    initMatrix[audi][lamborguini] = "B"
+                
+                elif lamborguini % 2 == 1:
+                    initMatrix[audi][lamborguini] = "N"  
+logic()   
+
+
+def show():
+
+    for z in range(len(initMatrix)):
+        print(end=f"\nF{z + 1}. ")
+
+        for w in range(len(initMatrix[z])):
+
+            if w % 2 == 0:
+                print(end=f"| {initMatrix[z][w]} |")
+
+            elif w == len(initMatrix[z]) - 1:
+                print(end=f" {initMatrix[z][w]} |")
+
+            elif w % 2 == 1:
+                print(end=f" {initMatrix[z][w]} ")
+
+        if z == len(initMatrix) - 1:
+            print()
+show()
+
+print("\nBuscar Posición Matricial!")
+
+while True:
     
-#*--- + --- + --- + ---\ PRINT \--- + --- + --- + ---*#
-print(f"\n[ {x} x {y} ]")
-for z in range(len(initMatrix)):
-    print()
+    x = int(input("\nIngresa Posición [FILA]: "))
+    y = int(input("Ingresa Posición [COLUMNA]: "))
 
-    for w in range(len(initMatrix[z])):
-        if w % 2 == 0:
-            print(end=f"| {initMatrix[z][w]} |")
+    if x <= len(initMatrix) and y <= len(initMatrix[0]):
+        for camaro in range(len(initMatrix)):
+            for astonMartin in range(len(initMatrix[camaro])):
 
-        elif w == len(initMatrix[z]) - 1:
-            print(end=f" {initMatrix[z][w]} |")
+                if camaro == (x - 1) and astonMartin == (y - 1):
+                    initMatrix[camaro][astonMartin] = initMatrix[camaro][astonMartin]
+                
+                else:
+                    initMatrix[camaro][astonMartin] = "-"
+        show()
+        print(f"[{x}, {y}]")   
 
-        elif w % 2 == 1:
-            print(end=f" {initMatrix[z][w]} ")
+    else:
+        print("\nIngresa Rangos Permitidos!")
 
+    logic()
