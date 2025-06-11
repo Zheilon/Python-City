237abc534e2844afaae979fb10a96e66b0de1927
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2025-06-10 23:13:28 -0500

    Algoritmo | Juego matricial: Triqui. Person vs PC -V1.0

--- exercises/info.py
+++
@@ -1,3 +0,0 @@
-print('''Nombre: Juan Esteban Torres Pachón.
-      Materia: Programación 1
-      Correo: esteban67pachon@outlook.com''')
--- exercises/transformacion_data.py
+++
@@ -1,8 +0,0 @@
-user = str(input("Digíte su nombre completo: "))
-cedula = int(input("Digíte su Cédula: "))
-estatura = float(input("Digíte su Altura en Mts: "))
-estado = bool(input("Estado (True | False): "))
-
-print()
-
-print(f"Usuario: {user}\nCédula: {cedula}\nEstatura: {estatura}\nEstado Civil: {estado}")
